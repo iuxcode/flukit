9d4b39736fb7625ccb3308589fba6cd6a501a37e
charles9904 <charlottabade@gmail.com>
2022-07-11 14:22:23 +0000

    fix systemUIOverlay styles

--- lib/src/screens/base.dart
+++ lib/src/screens/base.dart
@@ -11,31 +11,45 @@ class FluScreen extends StatelessWidget {
   final Color? backgroundColor;
   final FloatingActionButtonLocation floatingActionButtonLocation;
 
-  const FluScreen({
-    Key? key,
-    required this.body,
-    this.appBar,
-    this.systemUiOverlayStyle,
-    this.bottomNavigationBar,
-    this.floatingActionButton,
-    this.extendBody = false,
-    this.backgroundColor,
-    this.floatingActionButtonLocation = FloatingActionButtonLocation.centerDocked
-  }) : super(key: key);
+  const FluScreen(
+      {Key? key,
+      required this.body,
+      this.appBar,
+      this.systemUiOverlayStyle,
+      this.bottomNavigationBar,
+      this.floatingActionButton,
+      this.extendBody = false,
+      this.backgroundColor,
+      this.floatingActionButtonLocation =
+          FloatingActionButtonLocation.centerDocked})
+      : super(key: key);
+
+  SystemUiOverlayStyle get defaultSystemStyle => Flukit.systemOverlayStyle;
 
   @override
   Widget build(BuildContext context) => AnnotatedRegion<SystemUiOverlayStyle>(
-    value: Flukit.systemOverlayStyle.copyWith(
-      statusBarColor: Colors.transparent
-    ),
-    child: Scaffold(
-      backgroundColor: backgroundColor ?? Flukit.theme.backgroundColor,
-      extendBody: extendBody,
-      appBar: appBar,
-      body: body,
-      bottomNavigationBar: bottomNavigationBar,
-      floatingActionButtonLocation: floatingActionButtonLocation,
-      floatingActionButton: floatingActionButton,
-    )
-  );
+      value: defaultSystemStyle.copyWith(
+        statusBarColor: systemUiOverlayStyle?.statusBarColor,
+        statusBarIconBrightness: systemUiOverlayStyle?.statusBarIconBrightness,
+        statusBarBrightness: systemUiOverlayStyle?.statusBarBrightness,
+        systemNavigationBarColor:
+            systemUiOverlayStyle?.systemNavigationBarColor,
+        systemNavigationBarDividerColor:
+            systemUiOverlayStyle?.systemNavigationBarDividerColor,
+        systemNavigationBarIconBrightness:
+            systemUiOverlayStyle?.systemNavigationBarIconBrightness,
+        systemStatusBarContrastEnforced:
+            systemUiOverlayStyle?.systemStatusBarContrastEnforced,
+        systemNavigationBarContrastEnforced:
+            systemUiOverlayStyle?.systemNavigationBarContrastEnforced,
+      ),
+      child: Scaffold(
+        backgroundColor: backgroundColor ?? Flukit.theme.backgroundColor,
+        extendBody: extendBody,
+        appBar: appBar,
+        body: body,
+        bottomNavigationBar: bottomNavigationBar,
+        floatingActionButtonLocation: floatingActionButtonLocation,
+        floatingActionButton: floatingActionButton,
+      ));
 }
