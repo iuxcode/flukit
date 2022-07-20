69e1f6cc830a2a90211567f4e03b8da14ad0fc33
charles9904 <charlottabade@gmail.com>
2022-07-20 10:57:04 +0000

    fix modalBottomSheet

--- lib/src/widgets/modals.dart
+++ lib/src/widgets/modals.dart
@@ -6,63 +6,64 @@ class FluModalBottomSheet extends StatelessWidget {
   final EdgeInsets? padding;
   final double? radius;
   final Duration animationDuration;
-  final Curve  animationCurve;
-  final double defaultRadius =  Flukit.screenSize.width * .085;
+  final Curve animationCurve;
+  final double defaultRadius = Flukit.screenSize.width * .085;
 
-  FluModalBottomSheet({
-    Key? key,
-    required this.child,
-    this.padding,
-    this.radius,
-    this.animationDuration = const Duration(milliseconds: 300),
-    this.animationCurve = Curves.easeInOut
-  }) : super(key: key);
+  FluModalBottomSheet(
+      {Key? key,
+      required this.child,
+      this.padding,
+      this.radius,
+      this.animationDuration = const Duration(milliseconds: 300),
+      this.animationCurve = Curves.easeInOut})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) => Padding(
-    padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
-    child: DraggableScrollableSheet(
-      expand: false,
-      maxChildSize: .9,
-      builder: (BuildContext context, ScrollController scrollController) {
-        return Column(
-          // mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            Expanded(child: GestureDetector(
+        padding:
+            EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+        child: DraggableScrollableSheet(
+            expand: false,
+            maxChildSize: .9,
+            builder: (BuildContext context, ScrollController scrollController) {
+              return Column(
+                // mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  /* Expanded(child: GestureDetector(
               onTap: () => Navigator.pop(context),
-            )),
-            AnimatedContainer(
-              height: 4,
-              width: Flukit.screenSize.width * .20,
-              margin: const EdgeInsets.only(bottom: 8),
-              duration: animationDuration,
-              curve: animationCurve,
-              decoration: BoxDecoration(
-                color: Flukit.theme.backgroundColor.withOpacity(.9),
-                borderRadius: BorderRadius.circular(10)
-              ),
-            ),
-            AnimatedContainer(
-              width: double.infinity,
-              duration: animationDuration,
-              curve: animationCurve,
-              decoration: BoxDecoration(
-                color: Flukit.theme.backgroundColor,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(radius ?? defaultRadius),
-                  topRight: Radius.circular(radius ?? defaultRadius)
-                ),
-              ),
-              child: SingleChildScrollView(
-                controller: scrollController,
-                padding: padding ?? EdgeInsets.all(Flukit.appConsts.defaultPaddingSize),
-                child: child
-              ),
-            ),
-          ],
-        );
-      }
-    ),
-  );
+            )), */
+                  AnimatedContainer(
+                    height: 4,
+                    width: Flukit.screenSize.width * .20,
+                    margin: const EdgeInsets.only(bottom: 8),
+                    duration: animationDuration,
+                    curve: animationCurve,
+                    decoration: BoxDecoration(
+                        color: Flukit.theme.backgroundColor.withOpacity(.9),
+                        borderRadius: BorderRadius.circular(10)),
+                  ),
+                  Expanded(
+                    child: AnimatedContainer(
+                      width: double.infinity,
+                      duration: animationDuration,
+                      curve: animationCurve,
+                      decoration: BoxDecoration(
+                        color: Flukit.theme.backgroundColor,
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(radius ?? defaultRadius),
+                            topRight: Radius.circular(radius ?? defaultRadius)),
+                      ),
+                      child: SingleChildScrollView(
+                          controller: scrollController,
+                          padding: padding ??
+                              EdgeInsets.all(
+                                  Flukit.appConsts.defaultPaddingSize),
+                          child: child),
+                    ),
+                  ),
+                ],
+              );
+            }),
+      );
 }
