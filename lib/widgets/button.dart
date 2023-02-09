1cda812e2c29c00329e43a6e54936b08ba82cef9
charles9904 <charlottabade@gmail.com>
2023-02-09 16:38:32 +0000

    better button widget

--- lib/widgets/button.dart
+++ lib/widgets/button.dart
@@ -1,30 +1,67 @@
 import 'package:flutter/material.dart';
 import 'package:flukit_icons/flukit_icons.dart';
 import 'package:get/get.dart';
+import '../utils/flu_utils.dart';
 
 /// Create a button
 class FluButton extends StatelessWidget {
   const FluButton({
     required this.child,
     required this.onPressed,
-    this.style,
+    this.builder,
+    this.backgroundColor,
+    this.foregroundColor,
+    this.padding = EdgeInsets.zero,
+    this.margin = EdgeInsets.zero,
+    this.elevation = 0.0,
+    this.filled = false,
+    this.flat = false,
     super.key,
   });
 
+  final VoidCallback? onPressed;
+  final Color? backgroundColor;
+  final Color? foregroundColor;
+  final EdgeInsets padding;
+  final EdgeInsets margin;
+  final double elevation;
+  final bool filled;
+  final bool flat;
+  final Widget? child;
+  final Widget Function(BuildContext)? builder;
+
   /// Create a button with icon content
   factory FluButton.icon(FluIcons icon,
-      {VoidCallback? onPressed, double iconSize = 20, FluButtonStyle? style}) {
-    final iconButtonStyle = style ?? FluButtonStyle.tonal(Get.context!);
-
-    return FluButton(
+          {double iconSize = 20,
+          double iconStrokeWidth = 1.5,
+          FluIconStyles iconStyle = FluIconStyles.twotone,
+          VoidCallback? onPressed,
+          Color? backgroundColor,
+          Color? foregroundColor,
+          EdgeInsets padding = EdgeInsets.zero,
+          EdgeInsets margin = EdgeInsets.zero,
+          double elevation = 0.0,
+          bool filled = false,
+          bool flat = false}) =>
+      FluButton(
         onPressed: onPressed,
-        style: iconButtonStyle,
-        child: FluIcon(
+        backgroundColor: backgroundColor,
+        foregroundColor: foregroundColor,
+        padding: padding,
+        margin: margin,
+        elevation: elevation,
+        filled: filled,
+        flat: flat,
+        builder: (context) => FluIcon(
           icon,
-          color: iconButtonStyle.foregroundColor,
           size: iconSize,
-        ));
-  }
+          style: iconStyle,
+          strokewidth: iconStrokeWidth,
+          color: _getButtonForegroundColor(context,
+              flat: flat, filled: filled, disabled: onPressed == null),
+        ),
+        child: null,
+      );
 
   /// Create a button with text content
   /// You can also add an icon before or after the text using [prefixIcon] and [suffixIcon].
@@ -37,65 +74,95 @@ class FluButton extends StatelessWidget {
       double iconSize = 20,
       double? prefixIconSize,
       double? suffixIconSize,
-      double spacing = 6.0,
-      VoidCallback? onPressed,
       FluIconStyles iconStyle = FluIconStyles.twotone,
-      FluButtonStyle? style}) {
-    final textButtonStyle = style ?? FluButtonStyle.tonal(Get.context!);
-
-    Widget textWidget =
-        Text(text, style: TextStyle(color: textButtonStyle.foregroundColor));
-    Widget buildIcon(FluIcons icon, [double? size]) => FluIcon(
-          icon,
-          style: iconStyle,
-          size: size ?? iconSize,
-          color: textButtonStyle.foregroundColor,
-          margin: EdgeInsets.only(
-            right: prefixIcon != null ? spacing : 0,
-            left: suffixIcon != null ? spacing : 0,
-          ),
-        );
-
+      double gap = 6.0,
+      VoidCallback? onPressed,
+      Color? backgroundColor,
+      Color? foregroundColor,
+      EdgeInsets padding = EdgeInsets.zero,
+      EdgeInsets margin = EdgeInsets.zero,
+      double elevation = 0.0,
+      bool filled = false,
+      bool flat = false}) {
     return FluButton(
       onPressed: onPressed,
-      style: textButtonStyle,
-      child: (prefixIcon != null || suffixIcon != null)
-          ? Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                if (prefixIcon != null) buildIcon(prefixIcon, prefixIconSize),
-                textWidget,
-                if (suffixIcon != null) buildIcon(suffixIcon, suffixIconSize),
-              ],
-            )
-          : textWidget,
+      backgroundColor: backgroundColor,
+      foregroundColor: foregroundColor,
+      padding: padding,
+      margin: margin,
+      elevation: elevation,
+      filled: filled,
+      flat: flat,
+      builder: (context) {
+        Color foregroundColor = _getButtonForegroundColor(context,
+            flat: flat, filled: filled, disabled: onPressed == null);
+        Widget buildIcon(FluIcons icon, [double? size]) => FluIcon(
+              icon,
+              style: iconStyle,
+              size: size ?? iconSize,
+              color: foregroundColor,
+              margin: EdgeInsets.only(
+                right: prefixIcon != null ? gap : 0,
+                left: suffixIcon != null ? gap : 0,
+              ),
+            );
+
+        Widget textWidget =
+            Text(text, style: TextStyle(color: foregroundColor));
+
+        return (prefixIcon != null || suffixIcon != null)
+            ? Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  if (prefixIcon != null) buildIcon(prefixIcon, prefixIconSize),
+                  textWidget,
+                  if (suffixIcon != null) buildIcon(suffixIcon, suffixIconSize),
+                ],
+              )
+            : textWidget;
+      },
+      child: null,
     );
   }
 
-  final Widget child;
-  final VoidCallback? onPressed;
-
-  /// Button style
-  /// Default value is [FluButtonStyle.tonal]
-  final FluButtonStyle? style;
-
   @override
   Widget build(BuildContext context) {
-    final buttonStyle = FluButtonStyle.tonal(context).merge(style);
-
-    final Widget button = TextButton(
-      onPressed: onPressed,
-      style: ButtonStyle(
-        backgroundColor: MaterialStateProperty.all(buttonStyle.backgroundColor),
-        foregroundColor: MaterialStatePropertyAll(buttonStyle.foregroundColor),
-        padding: MaterialStatePropertyAll(buttonStyle.padding),
-      ),
-      child: child,
+    ButtonStyle buttonStyle = ButtonStyle(
+      backgroundColor: MaterialStateProperty.all(backgroundColor),
+      foregroundColor: MaterialStatePropertyAll(foregroundColor),
+      padding: MaterialStatePropertyAll(padding),
+      elevation: MaterialStatePropertyAll(elevation),
     );
+    Widget? child = this.child ?? builder?.call(context);
+    Widget button;
 
-    if (buttonStyle.margin != EdgeInsets.zero) {
+    if (elevation > 0 && !flat && !filled) {
+      button = ElevatedButton(
+          onPressed: onPressed,
+          style: buttonStyle,
+          child: child ?? const Text('Elevated button !'));
+    } else if (flat) {
+      button = TextButton(
+          onPressed: onPressed,
+          style: buttonStyle,
+          child: child ?? const Text('Text button !'));
+    } else if (filled) {
+      button = FilledButton(
+        onPressed: onPressed,
+        style: buttonStyle,
+        child: child ?? const Text('Filled button !'),
+      );
+    } else {
+      button = FilledButton.tonal(
+        onPressed: onPressed,
+        style: buttonStyle,
+        child: child ?? const Text('Tonal button !'),
+      );
+    }
+
+    if (margin != EdgeInsets.zero) {
       return Container(
-        margin: buttonStyle.margin,
+        margin: margin,
         child: button,
       );
     }
@@ -104,77 +171,23 @@ class FluButton extends StatelessWidget {
   }
 }
 
-/// Create a [FluButton] style in order to customize it easily.
-@immutable
-class FluButtonStyle {
-  const FluButtonStyle({
-    this.backgroundColor,
-    this.foregroundColor,
-    this.padding = EdgeInsets.zero,
-    this.margin = EdgeInsets.zero,
-    this.square = false,
-    this.height,
-    this.width,
-    this.minWidth,
-  });
+/// Return foreground color according to M3 button specs
+/// https://m3.material.io/components/buttons/specs
+Color _getButtonForegroundColor(BuildContext context,
+    {required bool flat,
+    required bool filled,
+    required bool disabled,
+    double elevation = 0.0}) {
+  ColorScheme colorScheme = Theme.of(context).colorScheme;
 
-  static FluButtonStyle tonal(BuildContext context) {
-    ColorScheme colorScheme = Theme.of(context).colorScheme;
-
-    return FluButtonStyle(
-        backgroundColor: colorScheme.primaryContainer,
-        foregroundColor: colorScheme.onPrimaryContainer);
-  }
-
-  /// Button background color
-  final Color? backgroundColor;
-
-  /// Button foreground color
-  final Color? foregroundColor;
-
-  /// Button Height
-  final double? height;
-
-  /// The padding between the button's boundary and its child
-  final EdgeInsets margin;
-
-  /// Button minimum width
-  final double? minWidth;
-
-  /// Empty space to surround the button
-  final EdgeInsets padding;
-
-  /// if it's set to true, the button'll be a square.
-  /// so the button height will be equal to the width.
-  final bool square;
-
-  /// Button width
-  final double? width;
-
-  /// Creates a copy of this style but with the given fields replaced with the new values.
-  FluButtonStyle copyWith({
-    EdgeInsets? margin,
-    EdgeInsets? padding,
-    Color? backgroundColor,
-    Color? foregroundColor,
-    bool? square,
-  }) =>
-      FluButtonStyle(
-        margin: margin ?? this.margin,
-        padding: padding ?? this.padding,
-        backgroundColor: backgroundColor ?? this.backgroundColor,
-        foregroundColor: foregroundColor ?? this.foregroundColor,
-        square: square ?? this.square,
-      );
-
-  /// Merge this style with the given one.
-  FluButtonStyle merge(FluButtonStyle? style) => FluButtonStyle(
-        margin: style?.margin ?? margin,
-        padding: style?.padding ?? padding,
-        backgroundColor: style?.backgroundColor ?? backgroundColor,
-        foregroundColor: style?.foregroundColor ?? foregroundColor,
-        square: style?.square ?? square,
-      );
+  if (disabled)
+    return colorScheme.onSurface;
+  else if (elevation > 0 && !flat && !filled)
+    return colorScheme.onBackground;
+  else if (flat)
+    return colorScheme.primary;
+  else if (filled)
+    return colorScheme.onPrimary;
+  else
+    return colorScheme.onSecondaryContainer;
 }
-
-enum FluButtonType { elevated, filled, tonal, outlined, text }
