9e821b9ba4fb1b4e3409e5623f546f0ebea98b62
charles9904 <charlottabade@gmail.com>
2023-04-01 10:22:34 +0000

    remove materialApp override.

--- lib/utils/theme.dart
+++ lib/utils/theme.dart
@@ -21,3 +21,33 @@ class FluTheme {
   ThemeData get dark => light.copyWith(
       colorScheme: darkColors ?? ColorScheme.fromSeed(seedColor: seedColor!));
 }
+
+/* class FluTheme {
+  FluTheme({
+    this.colorSchemeSeed,
+    this.lightColorScheme,
+    this.darkColorScheme,
+  });
+
+  final Color? colorSchemeSeed;
+  final ColorScheme? darkColorScheme;
+  final ColorScheme? lightColorScheme;
+
+  ThemeData get light => _buildTheme(lightColorScheme ??
+      ColorScheme.fromSeed(
+        seedColor: colorSchemeSeed ?? _defaultSeedColor,
+        brightness: Brightness.light,
+      ));
+  ThemeData get dark => _buildTheme(darkColorScheme ??
+      ColorScheme.fromSeed(
+        seedColor: colorSchemeSeed ?? _defaultSeedColor,
+        brightness: Brightness.dark,
+      ));
+
+  ThemeData _buildTheme(ColorScheme colorScheme) => ThemeData(
+        colorScheme: colorScheme,
+        useMaterial3: true,
+      );
+}
+
+const Color _defaultSeedColor = Color(0xFF1651E7); */
--- lib/widgets/app.dart
+++ lib/widgets/app.dart
@@ -1,70 +1,25 @@
 import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
 
-import '../data/providers/locales.dart';
 import '../data/providers/theme_provider.dart';
 import '../data/repositories/theme_repository.dart';
-import '../navigation/router.dart';
-import '../screens/default.dart';
 import '../utils/theme.dart';
 
 class FluApp extends StatelessWidget {
   const FluApp({
-    this.title,
-    this.home,
-    this.supportedLocales = const <Locale>[Locale('en', 'US')],
+    required this.builder,
     this.theme = FluDefaultThemes.blue,
     this.themeMode = ThemeMode.system,
     this.useSystemTheme = false,
-    this.debugShowCheckedModeBanner = false,
-    this.routes = const [],
     super.key,
   });
 
-  final Widget? home;
-  final List<Locale> supportedLocales;
   final FluTheme theme;
   final ThemeMode themeMode;
-  final String? title;
   final bool useSystemTheme;
-  final bool debugShowCheckedModeBanner;
-  final List<RouteBase> routes;
-
-  Widget _buildApp(
-      {required ThemeData theme,
-      required ThemeData darkTheme,
-      required ThemeMode themeMode,
-      required Locale locale}) {
-    List<RouteBase> _routes = routes;
-
-    if (_routes.isEmpty) {
-      _routes = [
-        FluRoute(
-          name: "flukit_default",
-          path: "/flukit_default",
-          builder: (context, state) => const FluDefaultScreen(),
-        )
-      ];
-    }
-
-    return MaterialApp.router(
-      title: title ?? "",
-      debugShowCheckedModeBanner: debugShowCheckedModeBanner,
-      theme: theme,
-      darkTheme: darkTheme,
-      themeMode: themeMode,
-      locale: locale,
-      supportedLocales: supportedLocales,
-      localizationsDelegates: const [
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-      ],
-      routerConfig: FluRouter(_routes).getRouterConfig(),
-    );
-  }
+  final Widget Function(
+      BuildContext context, FluTheme theme, ThemeMode themeMode) builder;
 
   @override
   Widget build(BuildContext context) {
@@ -77,7 +32,6 @@ class FluApp extends StatelessWidget {
       child: Consumer(
         builder: (context, ref, child) {
           final themeManager = ref.watch(themeProvider);
-          final locale = ref.watch(localesProvider);
 
           FluTheme appTheme = themeManager.currentTheme;
 
@@ -89,22 +43,12 @@ class FluApp extends StatelessWidget {
                       lightColors: lightColors, darkColors: darkColors);
                 }
 
-                return _buildApp(
-                  theme: appTheme.light,
-                  darkTheme: appTheme.dark,
-                  themeMode: themeManager.themeMode,
-                  locale: locale,
-                );
+                return builder.call(context, appTheme, themeManager.themeMode);
               },
             );
           }
 
-          return _buildApp(
-            theme: appTheme.light,
-            darkTheme: appTheme.dark,
-            themeMode: themeManager.themeMode,
-            locale: locale,
-          );
+          return builder.call(context, appTheme, themeManager.themeMode);
         },
       ),
     );
