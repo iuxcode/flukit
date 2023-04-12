31d483b40ee3fb5bd37931fd9bcc2db0367148de
charles9904 <charlottabade@gmail.com>
2023-04-12 05:17:01 +0000

    optimization

--- lib/flukit.dart
+++ lib/flukit.dart
@@ -1,6 +1,8 @@
 library flukit;
 
+export 'package:geolocator/geolocator.dart' show Position, LocationPermission;
+export 'package:flukit_icons/flukit_icons.dart';
+export 'utils/flu_utils.dart';
 export 'widgets/flu_widgets.dart';
 export 'screens/flu_screens.dart';
-export 'utils/flu_utils.dart';
-export 'package:flukit_icons/flukit_icons.dart';
+export 'services/flu_services.dart';
